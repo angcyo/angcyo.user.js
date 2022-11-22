2eef19e5c602d2de7585797ddb3ace14fb260447
angcyo <angcyo@126.com>
2022-11-22 18:55:19 +0800

    init

---
+++ angcyo.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        angcyo
+// @namespace   angcyo
+// @match       http://*
+// @match       https://*
+// @grant       none
+// @version     1.0
+// @author      angcyo
+// @description 2022-11-22
+// ==/UserScript==
