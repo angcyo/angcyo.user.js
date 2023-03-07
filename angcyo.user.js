4b8f092d9fea2a8125218771840b8b05cf92e93e
angcyo <angcyo@126.com>
2023-03-07 11:41:28 +0800

    0.8

--- angcyo.user.js
+++ angcyo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         angcyo
 // @namespace    https://github.com/angcyo
-// @version      0.7
+// @version      0.8
 // @description  2022-11-22
 // @author       angcyo
 // @license      MIT License
